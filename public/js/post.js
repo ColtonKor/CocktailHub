b82892cc19feb86261655fd989fb79f96d4d082c
ColtonKor <ckorhummel@csumb.edu>
2024-12-03 18:26:52 -0800

    Made comments be toggleable

--- public/js/post.js
+++ public/js/post.js
@@ -6,10 +6,31 @@ document.querySelector('.btn-close').addEventListener('click', function () {
 
 
 document.querySelector('#newPost').addEventListener('click', openModal);
-
-
 function openModal() {
   const myModal = new bootstrap.Modal('#newPostModal');
   myModal.show();
+}
+
+let togglePause = document.querySelectorAll(".toggle");
+for(let i of togglePause){
+  i.addEventListener("click", openComments);
+}
+
+let commentVis = document.querySelectorAll(".comments-section");
+for(let i of commentVis){
+  i.style.display = 'none';
+}
+
+function openComments(){
+  const post = event.target.closest('.post');
+  const commentsSection = post.querySelector('.comments-section');
+  const toggleButton = event.target;
+  if (commentsSection.style.display === 'none' || commentsSection.style.display === '') {
+    commentsSection.style.display = 'block';
+    toggleButton.textContent = 'Close Comments';
+  } else {
+    commentsSection.style.display = 'none';
+    toggleButton.textContent = 'Open Comments';
+  }
 }
   
