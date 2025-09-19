aa761a1ebb0b653cd75303b9fc394614f769bb74
ColtonKor <ckorhummel@csumb.edu>
2025-09-19 11:39:24 -0700

    The fastcomet is gone
    I updated this project to use a supabase database which I will keep on incase any of us want to showcase this project for a portfolio or anything like that.

--- index.mjs
+++ index.mjs
@@ -3,6 +3,9 @@ import mysql from 'mysql2/promise';
 import fetch from 'node-fetch';
 import bcrypt from "bcrypt";
 import session from 'express-session';
+import pkg from "pg";
+
+const { Pool } = pkg;
 
 const app = express();
 app.use(express.json());
@@ -17,15 +20,20 @@ app.use(session({
   saveUninitialized: true,
 }))
 
-const pool = mysql.createPool({
-    host: "aureliano-khoury.tech",
-    user: "aurelia1_webuser",
-    password: "100webuser",
-    database: "aurelia1_quotes",
-    connectionLimit: 10,
-    waitForConnections: true
+const conn = new Pool({
+  user: "postgres.vgrkuwvymsgdavyvhpoq",
+  host: "aws-1-us-west-1.pooler.supabase.com",
+  database: "postgres",
+  password: "ColtonDatabasePassword1",
+  port: 6543,
 });
-const conn = await pool.getConnection();
+// const conn = await pool.getConnection();
+conn.connect()
+  .then(client => {
+    console.log("✅ Connected to PostgreSQL");
+    client.release();
+  })
+  .catch(err => console.error("❌ Database connection error:", err.stack));
 
 async function fetchCocktails() {
     let response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
@@ -83,23 +91,27 @@ app.get('/cocktail/:name', async (req, res) => {
 
 app.post('/like', async (req, res) => {
     const { postId } = req.body;
-    let getLikesSql = 'SELECT likes FROM Posts WHERE postId = ?';
+    let getLikesSql = 'SELECT likes FROM Posts WHERE postid = $1';
     let sqlParamsLike = [postId];
-    const [likesTable] = await conn.query(getLikesSql, sqlParamsLike);
-    let sql = 'UPDATE Posts SET likes = ? WHERE postId = ?';
+    const likeTable = await conn.query(getLikesSql, sqlParamsLike);
+    const likesTable = likeTable.rows;
+    let sql = 'UPDATE Posts SET likes = $1 WHERE postid = $2';
     let sqlParams = [likesTable[0].likes + 1, postId]
-    const [rows] = await conn.query(sql, sqlParams);
+    const row = await conn.query(sql, sqlParams);
+    const rows = row.rows;
     res.redirect('/posts');
 });
 
 app.post('/likeComment', async (req, res) => {
     const { commentId } = req.body;
-    let getLikesSql = 'SELECT likes FROM Comments WHERE commentId = ?';
+    let getLikesSql = 'SELECT likes FROM Comments WHERE commentid = $1';
     let sqlParamsLike = [commentId];
-    const [likesTable] = await conn.query(getLikesSql, sqlParamsLike);
-    let sql = 'UPDATE Comments SET likes = ? WHERE commentId = ?';
+    const likeTable = await conn.query(getLikesSql, sqlParamsLike);
+    const likesTable = likeTable.rows;
+    let sql = 'UPDATE Comments SET likes = $1 WHERE commentid = $2';
     let sqlParams = [likesTable[0].likes + 1, commentId]
-    const [rows] = await conn.query(sql, sqlParams);
+    const row = await conn.query(sql, sqlParams);
+    const rows = row.rows;
     res.redirect('/posts');
 });
 
@@ -109,41 +121,49 @@ app.post('/comment', async (req, res) => {
     const { commentContent } = req.body;
     let currentDate = new Date();
     let formattedDate = currentDate.toISOString().split('T')[0];
-    let sql = 'INSERT INTO Comments (text, likes, userId, postId, datePosted) VALUES (?,?,?,?,?)';
+    let sql = 'INSERT INTO Comments (text, likes, userid, postid, dateposted) VALUES ($1, $2, $3, $4, $5)';
     let sqlParams = [commentContent, 0, req.session.user.id, postId, formattedDate];
-    const [rows] = await conn.query(sql, sqlParams);
+    const row = await conn.query(sql, sqlParams);
+    const rows = row.rows;
     res.redirect('/posts');
 });
 
 app.get('/posts', isAuthenticated, async (req, res) => {
+    console.log("Hello: " + req.session.user.id);
     let keyword = req.query.keyword || '';
-    let sql = 'SELECT * FROM Posts NATURAL JOIN users WHERE username LIKE ? ORDER BY postId DESC';
+    let sql = 'SELECT * FROM Posts NATURAL JOIN users WHERE username LIKE $1 ORDER BY postid DESC';
     let postSearch = [`%${keyword}%`];
     const drinks = await fetchCocktails();
-    const [posts] = await conn.query(sql, postSearch);
-    let UserSql = `SELECT * FROM users WHERE userId = ?`;
+    const post = await conn.query(sql, postSearch);
+    const posts = post.rows;
+    let UserSql = `SELECT * FROM users WHERE userid = $1`;
     let sqlParams = [req.session.user.id];
-    const [user] = await conn.query(UserSql, sqlParams);
+    const users = await conn.query(UserSql, sqlParams);
+    const user = users.rows;
     let sqlComments = 'SELECT * FROM Comments NATURAL JOIN users';
-    const [comments] = await conn.query(sqlComments);
+    const comment = await conn.query(sqlComments);
+    const comments = comment.rows;
     console.log(posts);
     res.render('posts', { posts, drinks, user: req.session.user.id, comments});
 });
 
 app.post('/posts', async (req, res) => {
+    console.log("Hello: " + req.session.user.id);
     let drink = req.body.drinkList;
     let caption = req.body.caption;
     let content = `Drink: ${drink}`; 
     let cocktail = await fetchCocktailDetails(drink);
     let currentDate = new Date();
     let formattedDate = currentDate.toISOString().split('T')[0];
-    let sql = 'INSERT INTO Posts (userId, content, caption, likes, image, instructions, datePosted) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    let sql = 'INSERT INTO Posts (userid, content, caption, likes, image, instructions, dateposted) VALUES ($1, $2, $3, $4, $5, $6, $7)';
     let sqlParams = [req.session.user.id, content, caption, 0, cocktail.strDrinkThumb, cocktail.strInstructions, formattedDate];
 
-    let sqlUpdate = 'UPDATE users SET postCount = postCount + 1 WHERE userId = ?';
+    let sqlUpdate = 'UPDATE users SET postcount = postcount + 1 WHERE userid = $1';
     let sqlParamsUpdate = [req.session.user.id];
-    const [update] = await conn.query(sqlUpdate, sqlParamsUpdate);
-    const [posts] = await conn.query(sql, sqlParams);
+    const updates = await conn.query(sqlUpdate, sqlParamsUpdate);
+    const update = updates.rows;
+    const post = await conn.query(sql, sqlParams);
+    const posts = post.rows;
     res.redirect('/posts');
 });
 
@@ -154,12 +174,14 @@ app.get('/logout', (req, res) => {
  });
 
 app.get('/profile', isAuthenticated, async (req, res) => {
-    let sql = `SELECT * FROM Posts NATURAL JOIN users WHERE userId = ? ORDER BY postId DESC`
-    let sqlParams = req.session.user.id;
-    const [posts] = await conn.query(sql, sqlParams);
+    let sql = `SELECT * FROM Posts NATURAL JOIN users WHERE userid = $1 ORDER BY postid DESC`
+    let sqlParams = [req.session.user.id];
+    const post = await conn.query(sql, sqlParams);
+    const posts = post.rows;
 
     let sqlComments = 'SELECT * FROM Comments NATURAL JOIN users';
-    const [comments] = await conn.query(sqlComments);
+    const results = await conn.query(sqlComments);
+    const comments = results.rows;
     
     res.render('profile.ejs', {user: req.session.user, posts, comments});
  });
@@ -177,19 +199,21 @@ app.post('/profile/edit', isAuthenticated, async (req, res) => {
     req.session.user.lastName = lastName;
     req.session.user.username = username;
     let sql = `UPDATE users
-               SET firstName =?,
-               lastName = ?,
-               username = ?
-               WHERE userId = ?`;
+               SET firstname = $1,
+               lastname = $2,
+               username = $3
+               WHERE userid = $4`;
     let sqlParams = [firstName, lastName, username, userId];
-    const [userData] = await conn.query(sql, sqlParams);
+    const results = await conn.query(sql, sqlParams);
+    const userData = results.rows;
     res.redirect('/profile');
 });
 
 app.get('/profile/deletePost', isAuthenticated, async (req, res) => {
     let postId = req.query.postId;
-    let sql = `DELETE FROM Posts WHERE postId = ?`;
-    const [rows] = await conn.query(sql, [postId]);
+    let sql = `DELETE FROM Posts WHERE postid = $1`;
+    const results = await conn.query(sql, [postId]);
+    const rows = results.rows;
 
     res.redirect('/profile');
 });
@@ -197,8 +221,9 @@ app.get('/profile/deletePost', isAuthenticated, async (req, res) => {
 app.get('/comment/delete', isAuthenticated, async (req, res) => {
     let commentId = req.query.commentId;
     console.log(commentId);
-    let sql = `DELETE FROM Comments WHERE commentId = ?`;
-    const [rows] = await conn.query(sql, [commentId]);
+    let sql = `DELETE FROM Comments WHERE commentid = $1`;
+    const results = await conn.query(sql, [commentId]);
+    const rows = results.rows;
 
     res.redirect('/posts');
 });
@@ -215,9 +240,10 @@ app.post('/signup', async (req, res) => {
 
     let userSql = `SELECT * 
                    FROM users 
-                   WHERE username = ?`;
+                   WHERE username = $1`;
     let userParams = [username];
-    const [unique] = await conn.query(userSql, userParams);
+    const results = await conn.query(userSql, userParams);
+    const unique = results.rows;
     if (unique.length > 0) {
         return res.status(400).send('Username already taken.');
     }
@@ -228,16 +254,16 @@ app.post('/signup', async (req, res) => {
     let pfpURL = `https://robohash.org/${username}.png?set=set4`;
 
     let sql = `INSERT INTO users 
-               (firstName, 
-                lastName, 
+               (firstname, 
+                lastname, 
                 username, 
                 password,
-                profilePicture) 
-                VALUES(?, ?, ?, ?, ?)`;
+                profilepicture) 
+                VALUES($1, $2, $3, $4, $5)`;
     let sqlParams = [fName, lName, username, hashedPassword, pfpURL];
             
     await conn.query(sql, sqlParams);
-res.render('login.ejs')
+    res.render('login.ejs')
 });
 
 app.post('/login', async (req, res) => {
@@ -246,9 +272,10 @@ app.post('/login', async (req, res) => {
 
     let sql = `SELECT * 
     FROM users
-    WHERE username = ?`;
+    WHERE username = $1`;
     let sqlParams = [username];
-    const [rows] = await conn.query(sql, sqlParams);
+    const results = await conn.query(sql, sqlParams);
+    const rows = results.rows;
 
     let passwordHash;
     if(rows.length > 0) { 
@@ -262,11 +289,11 @@ app.post('/login', async (req, res) => {
     if(match) {
         req.session.authenticated = true;
         req.session.user = {
-            id: rows[0].userId,
+            id: rows[0].userid,
             username: rows[0].username,
-            firstName: rows[0].firstName,
-            lastName: rows[0].lastName,
-            pfp: rows[0].profilePicture
+            firstName: rows[0].firstname,
+            lastName: rows[0].lastname,
+            pfp: rows[0].profilepicture
         };
         res.render('welcome.ejs');
     } else {
